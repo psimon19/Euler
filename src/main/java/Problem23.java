bfda3fd469805e8f48fca5430753e424357cf30b
Peter Simon <psimon@gatech.edu>
2015-11-03 17:16:02 -0500

    Completed Problem 23

--- src/main/java/Problem23.java
+++ src/main/java/Problem23.java
@@ -1,3 +1,4 @@
+import java.util.HashSet;
 /**
 * Problem 23 - www.projecteuler.net
 * Find the sum of all numbers that are NOT the sum
@@ -5,23 +6,47 @@
 *
 * An abundant number is a number that is less than the sum of its
 * proper divisors
-* 
+*
 * @author Peter Simon
-* @version 1.0
+* @version 2.0
 */
 public class Problem23 {
 
 
     public static void main(String[] args) {
         System.out.println(abundant(12));
-        for (int i = 24; i < 28123; i++) {
-            for (int j = 24; j <= i; j++) {
-                if (abundant(i) && abundant(j)) {
-                    //System.out.println(i + j);
-                }
-                
+        int count = 0; //Counting the abundant numbers
+        int[] abundantNum = new int[6965]; //Size gotten from previous check
+        int sum = 0; //Sum of all numbers
+        HashSet<Integer> abundantSum = new HashSet<Integer>();
+
+        for (int i = 12; i < 28123; i++) {
+            if (abundant(i)) {// && i % 2 == 1) {
+                abundantNum[count] = i;
+                count++;
             }
         }
+        for (int i = 0; i < abundantNum.length; i++) {
+            for (int j = i; j < abundantNum.length; j++) {
+                if ((abundantNum[i] + abundantNum[j]) <= 28123) { 
+                    abundantSum.add(abundantNum[i] + abundantNum[j]);
+                }
+            }
+        }
+
+        boolean checkFlag = false;
+        for (int i = 0; i <= 28123; i++) {
+            for (Integer j : abundantSum) {
+                if (i == j.intValue()){
+                    checkFlag = true;
+                }
+            }
+            if (checkFlag == false) {
+                sum += i;
+            }
+            checkFlag = false;
+        }
+        System.out.println(sum);
         System.out.println("-Done-");
     }
 
